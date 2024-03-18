fd492fb0c4a74ca0854bc7b554c467e441a409e5
Luna <thgcboss@gmail.com>
2024-03-18 16:59:25 +0900

    * add 1.5 support
    version bump

--- toolkitcore/Utilities/CommandInterface.cs
+++ toolkitcore/Utilities/CommandInterface.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using ToolkitCore.Controllers;
-using ToolkitCore.Interfaces;
-using ToolkitCore.Models;
+﻿using System.Windows.Input;
 using Verse;
 
 namespace ToolkitCore.Utilities
