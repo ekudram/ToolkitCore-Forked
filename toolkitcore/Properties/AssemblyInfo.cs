eafa136f3c8f1d00b0dd89ca3265be7f0ee92ee8
ekudram <ekudram@hotmail.com>
2025-09-18 21:16:19 -0600

    Changed a discription

--- toolkitcore/Properties/AssemblyInfo.cs
+++ toolkitcore/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("toolkit-core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core mod for use with TwitchToolkit.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("toolkit-core")]
